f3f37fa24c498bc36d8781192b93f599bda82f71
Alexander Strong <alex.j.strong@gmail.com>
2017-05-11 15:04:06 -0400

    Initial commit

---
+++ app/build/intermediates/ndkBuild/arm/debug/obj/local/armeabi-v7a/objs/HelloARNative/helloar.o.d
@@ -0,0 +1,44 @@
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/build/intermediates/ndkBuild/arm/debug/obj/local/armeabi-v7a/objs/HelloARNative/helloar.o: \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/helloar.cc \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/ar.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/camera.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/base.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/matrix.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/imagetracker.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/augmenter.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/image.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/imagetarget.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/target.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/storage.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/frame.hpp \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/utility.hpp \
+ /Users/alexstrong/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/renderer.hpp
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/ar.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/camera.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/base.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/matrix.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/imagetracker.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/augmenter.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/image.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/imagetarget.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/target.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/storage.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/frame.hpp:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/../../../../../../package/include/easyar/utility.hpp:
+
+/Users/alexstrong/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/alexstrong/Downloads/EasyARSDKSamples/Android/HelloARMultiTarget_SingleTracker_Native/app/src/main/jni/renderer.hpp:
