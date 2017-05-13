ef2c4692e46b355842bdf012182a01c36e64d622
Yu Heng Chen <yuhchen21@gmail.com>
2017-05-13 03:43:44 -0400

    Another attempt at transparency

--- app/src/main/jni/renderer.cc
+++ app/src/main/jni/renderer.cc
@@ -67,8 +67,7 @@ void Renderer::render(const Matrix44F& projectionMatrix,
     // turn on semi-transparent
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    //glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
